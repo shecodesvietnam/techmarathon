b76e812b356a2799c5620aa755511a9c9a3c837b
Vu Huy <vuhuycto@gmail.com>
2020-06-02 22:28:58 +0700

    Updates

---
+++ precache-manifest.22e5c52942ccc4424b3194fc7c03b37a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac6ccc4366498b3e1543dc93d982b00",
+    "url": "/techmarathon/index.html"
+  },
+  {
+    "revision": "edc67a8ebe824a9bbff9",
+    "url": "/techmarathon/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "92b174411ec9c81e65eb",
+    "url": "/techmarathon/static/css/main.710c4d7b.chunk.css"
+  },
+  {
+    "revision": "edc67a8ebe824a9bbff9",
+    "url": "/techmarathon/static/js/2.6220467f.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/techmarathon/static/js/2.6220467f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b174411ec9c81e65eb",
+    "url": "/techmarathon/static/js/main.258bf435.chunk.js"
+  },
+  {
+    "revision": "7d869a80cb927d863ad0",
+    "url": "/techmarathon/static/js/runtime-main.c7b0b36d.js"
+  },
+  {
+    "revision": "8ccdb04538e8f0f6fdcc48170a21f9b2",
+    "url": "/techmarathon/static/media/ava2020.8ccdb045.png"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/techmarathon/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/techmarathon/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/techmarathon/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/techmarathon/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/techmarathon/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/techmarathon/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/techmarathon/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/techmarathon/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/techmarathon/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/techmarathon/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/techmarathon/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/techmarathon/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/techmarathon/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/techmarathon/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/techmarathon/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/techmarathon/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/techmarathon/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/techmarathon/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/techmarathon/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "fe77db86e301ca80c3e643724f5c09d8",
+    "url": "/techmarathon/static/media/logo-2020.fe77db86.png"
+  },
+  {
+    "revision": "e60e66086b201a3b863a9557179f4780",
+    "url": "/techmarathon/static/media/precoding-1.e60e6608.png"
+  },
+  {
+    "revision": "0791225ca207af60f151197bb2f6e872",
+    "url": "/techmarathon/static/media/precoding-2.0791225c.png"
+  },
+  {
+    "revision": "11b199e547400f28595b8dbaef5bb1ff",
+    "url": "/techmarathon/static/media/precoding-3.11b199e5.jpg"
+  }
+]);
